fa5bcaeed68d134b52bce1bc423419b8f5ec2d18
Debjyoti Das <38778089+debjyotidas@users.noreply.github.com>
2024-05-05 08:34:50 +0530

    AirPressure.jsx

--- src/components/AirPressure.jsx
+++ src/components/AirPressure.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Parallax } from 'react-next-parallax';
+
 
 function AirPressure({stats}) {
     return (
         <>
+            <Parallax offsetMultiplier={2} scale={1.2}>
             <div className='bg-slate-600 p-2 flex flex-col justify-start items-center text-transform scale-100 hover:scale-125 transition-transform duration-300 ease-in-out cursor-pointer'>
                 <h2 className='text-sm mt-2 text-slate-300'>Air Pressure</h2>
                     <div className="mt-2 text-slate-300">
@@ -11,6 +14,8 @@ function AirPressure({stats}) {
                         <span className="text-2xl">mb</span>
                     </div>       
             </div>
+            </Parallax>
+            
         </>
       )
 }
