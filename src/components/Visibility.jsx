a5320597e03b2056f9675f66ba9c7ef5eb636e4b
Debjyoti Das <38778089+debjyotidas@users.noreply.github.com>
2024-05-06 14:25:50 +0530

    Visibility.jsx

--- src/components/Visibility.jsx
+++ src/components/Visibility.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import { Parallax } from 'react-next-parallax';
 
 function Visibility({stats}) {
   return (
     <>
-    <Parallax offsetMultiplier={2} scale={1.2}>
         <div className='bg-slate-600 p-2 flex flex-col justify-start items-center text-transform scale-100 hover:scale-125 transition-transform duration-300 ease-in-out cursor-pointer'>
             <h2 className='text-sm mt-2 text-slate-300'>Visibility</h2>
                 <div className="mt-2 text-slate-300">
@@ -13,9 +11,7 @@ function Visibility({stats}) {
                     <span className="text-2xl">miles</span>
                 </div>
 
-        </div>
-    </Parallax>
-        
+        </div> 
     </>
   )
 }
